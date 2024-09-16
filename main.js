88d3e0a086342acf2e52caf78e6301c1880da389
nathanostop234 <163007786+nathanostop234@users.noreply.github.com>
2024-09-16 11:06:53 -0300

    ...

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
+const nuvem = document.querySelector(".nuvem");
 const startButton = document.querySelector(".start");
 const gameOverScreen = document.querySelector(".game-over");
 
@@ -49,8 +50,8 @@ if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
     audioStart.pause();
 
     gameOverSound.play();
-    clearInterval(loop);
 
+    clearInterval(loop);
     gameOverScreen.style.display = "flex"
   }
 },10);
